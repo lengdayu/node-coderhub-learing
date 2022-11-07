adf1f1a4b8a07a14c4841fbae6a66d8348c9b556
葫芦娃 <15195765056@sina.cn>
2022-11-07 23:50:03 +0800

    用户登录接口开发

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const useRouter = (app) => {
+  fs.readdirSync(__dirname).forEach((file) => {
+    if (file === "index.js") {
+      return;
+    } else {
+      const router = require(`./${file}`);
+      app.use(router.routes());
+      app.use(router.allowedMethods());
+    }
+  });
+};
+
+module.exports = useRouter;
